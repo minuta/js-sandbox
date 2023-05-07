370c352246dc6a025a4d1fcec86903b5d84dcbfb
minuta <gammamustang@gmail.com>
2023-05-07 21:30:28 +0200

    add a topic about functiob basics

---
+++ 03-function-scope/01-function-basics/script.js
@@ -0,0 +1,17 @@
+function hi() {
+    console.log('hello');
+}
+
+hi();
+
+function add(num1, num2) {
+    console.log('add :', num1 + num2);
+}
+
+add(4, 6);
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+console.log('sub :', sub(34, 5));

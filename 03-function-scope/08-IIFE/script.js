2c171a3ca3b7c3b54f8eaf95461fa4d7c6cfdcf1
minuta <gammamustang@gmail.com>
2023-05-13 21:49:32 +0200

    add a topic about IIFE

---
+++ 03-function-scope/08-IIFE/script.js
@@ -0,0 +1,13 @@
+
+// IIFE = Immediately Invoked Function Expressions
+
+(function () {
+    console.log('hello');
+})();
+
+// adding parameters
+(function (name) {
+    console.log('Hello', name);
+})('Jack');
+
+

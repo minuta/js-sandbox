42b72f3a01c133ba4e62883eace385a3b5e35d21
minuta <gammamustang@gmail.com>
2023-09-25 22:36:39 +0200

    add a challenge : FizzBuzz

---
+++ 05-iteration-array-methods/04-fizzbuzz-challenge/script.js
@@ -0,0 +1,18 @@
+
+"use strict";
+
+for (let i = 1; i <= 15; i++) {
+    if (i%3 == 0 && i%5 == 0) {
+        console.log("FizzBuzz");
+        continue;
+    }
+    if (i%3 == 0) {
+        console.log("Fizz");
+    }
+    if (i%5 == 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+}

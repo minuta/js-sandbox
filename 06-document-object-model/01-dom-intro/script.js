bc299b6e523961833efcfc0c52f07992929e1b80
minuta <gammamustang@gmail.com>
2023-10-06 22:35:29 +0200

    add an overview for DOM

---
+++ 06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// 1. what is the main JS object in browser?
+
+// 2. what is the second main JS object in browser for representing the page?
+
+// 3. write code for representing the HTML part of the body
+
+// 3. Write code to change the whole HTML body to h1 "Hello!"
+
+// 4. Given a DIV element with id "main", write code to access this DIV-element with two different methods
+
+// 5. Change h1 content in DIV with id "main"
+
+
+
+
+// 1. what is the main JS object in browser?
+// window
+
+// 2. what is the second main JS object in browser for representing the page?
+// document or window.document
+
+// 3. write code for representing the HTML part of the body
+// document.body.innerHTML
+
+// 3. Write code to change the whole HTML body to h1 "Hello!"
+// document.body.innerHTML="<h1>hello!</h1>"
+
+// 4. Given a DIV element with id "main", write code to access this DIV-element with two different methods
+// document.querySelector("#main")
+// document.getElementById("main");
+
+// 5. Change h1 content in DIV with id "main"
+// document.querySelector("#main h1").innerHTML = "Foo";
+

a4923c7a7b0d947cb16141819e27e06132d65b0b
minuta <gammamustang@gmail.com>
2023-10-14 13:00:51 +0200

    add a topic of traversing DOM elements

---
+++ 06-document-object-model/05-traversing-the-dom-elements/challenge.js
@@ -0,0 +1,25 @@
+
+// Challenges!
+
+// 1. How do you get children elements from the DIV of class "parent"?
+// const parentElement = document.querySelector('.parent');
+// console.log(parentElement.children);
+
+
+// 2. How do you get a parent element from a child (class .child)?
+// Get a parent from the "Child 1"
+
+// const childElement = document.querySelector('.child');
+// const parentElement = childElement.parentElement
+// console.log(parentElement);
+
+
+// How do u get the first child and last child from the element div (class "parent")?
+// const parent = document.querySelector('.parent');
+// const firstCh = parent.firstElementChild;
+// console.log(firstCh.innerText);
+
+// 3. How to get a next Sibling for the "child 1"
+// const childOne = document.querySelector('.child');
+// const sibling = childOne.nextElementSibling;
+// console.log(sibling.innerText);

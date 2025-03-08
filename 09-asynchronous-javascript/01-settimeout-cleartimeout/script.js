ecd19dae345b8f4c794651c1af8f573dc9952193
minuta <gammamustang@gmail.com>
2025-03-08 13:29:27 +0100

    add an explanation for the clearTimeOut and timerId

--- 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
+++ 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
@@ -1,21 +1,24 @@
 // setTimeout - takes in a callback and a time to wait until that callback is executed
 
-setTimeout(function () {
-    console.log('Hello from callback');
-}, 2000);
+// setTimeout(function () {
+//     console.log('Hello from callback');
+// }, 2000);
 
 // Named function
-setTimeout(changeText, 3000);
+// setTimeout(changeText, 3000);
 
+// change text in the h1 tag
 function changeText() {
     document.querySelector('h1').textContent = 'Hello from callback';
 }
 
-// clearTimeout() will clear a timer
+// with timerId we can later cancel the timer, see function clearTimeout() below
 const timerId = setTimeout(changeText, 3000);
 
+// here we are clearing the timer via a button click event
+// this will cancel the callback from executing : changeText() function
 document.querySelector('#cancel').addEventListener('click', () => {
     console.log(timerId);
     clearTimeout(timerId);
-    console.log('Timer Cancelled');
+    console.log('Timer Cancelled, no more callback');
 });
